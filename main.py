4c5931c392cd4ebbd3dbf061a52f49a5281a4107
Maddy-10 <mahadmunir10@gmail.com>
2021-02-13 17:03:30 +0500

    Changes

--- main.py
+++ main.py
@@ -130,31 +130,30 @@ def worldPopulation(edriver):
     #     print(country.text)
 
 
+def cookieClick(driver):
+    driver.get("https://orteil.dashnet.org/cookieclicker/")
+    driver.implicitly_wait(5)
+    cookie = driver.find_element_by_id("bigCookie")
+    cookie_count = driver.find_element_by_id("cookies")
+    items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, -1)]
 
-# def cookieClick(driver):
-#     driver.get("https://orteil.dashnet.org/cookieclicker/")
-#     driver.implicitly_wait(5)
-#     cookie = driver.find_element_by_id("bigCookie")
-#     cookie_count = driver.find_element_by_id("cookies")
-#     items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, -1)]
-#
-#     actions = ActionChains(driver)
-#     actions.click(cookie)
-#
-#     for i in range(20):
-#         actions.perform()
-#         count = int(cookie_count.text.split(" ")[0])
-#
-#         for item in items:
-#             value = int(item.text)
-#
-#             if value <= count:
-#                 upgrade_actions = ActionChains(driver)
-#                 upgrade_actions.move_to_element(item)
-#                 upgrade_actions.click()
-#                 upgrade_actions.perform()
-#
-#     driver.quit()
+    actions = ActionChains(driver)
+    actions.click(cookie)
+
+    for i in range(20):
+        actions.perform()
+        count = int(cookie_count.text.split(" ")[0])
+
+        for item in items:
+            value = int(item.text)
+
+            if value <= count:
+                upgrade_actions = ActionChains(driver)
+                upgrade_actions.move_to_element(item)
+                upgrade_actions.click()
+                upgrade_actions.perform()
+
+    driver.quit()
 
 
 if __name__ == '__main__':
